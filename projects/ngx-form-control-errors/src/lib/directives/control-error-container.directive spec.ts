d087cd3c34bf0f6ceb7a259f1208094c20527daf
BrianJVarley <brianvarley9@gmail.com>
2020-07-30 22:24:00 +0100

    Add more test specs

---
+++ projects/ngx-form-control-errors/src/lib/directives/control-error-container.directive spec.ts
@@ -0,0 +1,42 @@
+import { Component, Input } from '@angular/core';
+import { SpectatorDirective, createDirectiveFactory } from '@ngneat/spectator';
+
+import { ControlErrorContainerDirective } from './control-error-container.directive';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'get-container',
+  template: ''
+})
+class GetContainerComponent {
+  @Input()
+  anchor: ControlErrorContainerDirective;
+}
+
+describe('ControlErrorContainerDirective', () => {
+  let spectator: SpectatorDirective<ControlErrorContainerDirective>;
+  const createDirective = createDirectiveFactory({
+    directive: ControlErrorContainerDirective,
+    declarations: [GetContainerComponent]
+  });
+
+  beforeEach(() => {
+    spectator = createDirective(`
+      <div controlErrorContainer #anchor="controlErrorContainer"></div>
+      <get-container [anchor]="anchor"></get-container>
+    `);
+  });
+
+  it('should create', () => {
+    expect(spectator.directive).toBeTruthy();
+  });
+
+  it('should contain ViewContainerRef instance', () => {
+    expect(spectator.directive.vcr).toBeTruthy();
+  });
+
+  it('should be exported as `controlErrorContainer`', () => {
+    spectator.detectChanges();
+    expect(spectator.query(GetContainerComponent).anchor).toBe(spectator.directive);
+  });
+});
--- projects/ngx-form-control-errors/src/lib/providers/form-errors.ts
+++ projects/ngx-form-control-errors/src/lib/providers/form-errors.ts
@@ -4,7 +4,7 @@ import { InjectionToken } from '@angular/core';
 export const defaultErrors = {
   required: (error) => `This field is required`,
   minlength: ({ requiredLength, actualLength }) => `Expect ${requiredLength} but got ${actualLength}`
-}
+};
 
 export const FORM_ERRORS = new InjectionToken('FORM_ERRORS', {
   providedIn: 'root',
